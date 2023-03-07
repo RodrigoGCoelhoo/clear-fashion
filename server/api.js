7a7d6fb44689969c1580cca0b3a9809fb150b746
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2023-03-07 13:26:36 +0100

    feat: endpoint created to query product by id

--- server/api.js
+++ server/api.js
@@ -1,6 +1,8 @@
-const cors = require('cors');
-const express = require('express');
-const helmet = require('helmet');
+const cors = require("cors");
+const express = require("express");
+const helmet = require("helmet");
+
+const { connect, client } = require("./queries");
 
 const PORT = 8092;
 
@@ -8,14 +10,25 @@ const app = express();
 
 module.exports = app;
 
-app.use(require('body-parser').json());
+app.use(require("body-parser").json());
 app.use(cors());
 app.use(helmet());
 
-app.options('*', cors());
+app.options("*", cors());
 
-app.get('/', (request, response) => {
-  response.send({'ack': true});
+app.get("/", (request, response) => {
+  response.send({ ack: true });
+});
+
+app.get("/products/:id", async (request, response) => {
+  const mongo = await connect();
+
+  const product = await mongo.findProductById(request.params.id);
+  console.log("Products:", product);
+
+  await client.close();
+
+  response.send({ product: product });
 });
 
 app.listen(PORT);
