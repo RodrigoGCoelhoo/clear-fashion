75621683acfe184134c137d9de9c200cd6b52e27
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2023-03-07 13:04:36 +0100

    feat: all methods running

--- server/queriesSandbox.js
+++ server/queriesSandbox.js
@@ -3,17 +3,17 @@ const { connect, client } = require("./queries");
 async function main() {
   const mongo = await connect();
 
-  //   const productsByBrand = await mongo.findProductsByBrand("DEDICATED");
-  //   console.log("Products by brand DEDICATED:", productsByBrand);
+  const productsByBrand = await mongo.findProductsByBrand("DEDICATED");
+  console.log("Products by brand DEDICATED:", productsByBrand);
 
-  //   const productsLessThanPrice = await mongo.findProductsLessThanPrice(50);
-  //   console.log("Products less than 50€:", productsLessThanPrice);
+  const productsLessThanPrice = await mongo.findProductsLessThanPrice(50);
+  console.log("Products less than 50€:", productsLessThanPrice);
 
-  //   const productsSortedByPrice = await mongo.findProductsSortedByPrice();
-  //   console.log("Products sorted by price:", productsSortedByPrice);
+  const productsSortedByPrice = await mongo.findProductsSortedByPrice();
+  console.log("Products sorted by price:", productsSortedByPrice);
 
-  //   const productsSortedByDate = await mongo.findProductsSortedByDate();
-  //   console.log("Products sorted by date:", productsSortedByDate);
+  const productsSortedByDate = await mongo.findProductsSortedByDate();
+  console.log("Products sorted by date:", productsSortedByDate);
 
   const productsScrapedLessThanTwoWeeks = await mongo.findProductsScrapedLessThanTwoWeeks();
   console.log("Products scraped less than two weeks:", productsScrapedLessThanTwoWeeks);
